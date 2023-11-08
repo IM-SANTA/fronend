8b18b03292163882be3343a1dbfabbd0189eae75
Hannah Ahn <ahnlook90@gmail.com>
2023-11-08 15:32:30 +0900

    feat: 산타극장 상세 정보 UI 구현

--- src/pages/movies/MovieDetail.tsx
+++ src/pages/movies/MovieDetail.tsx
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import YouTube from 'react-youtube';
 import Layout from '../Layout';
+import leftArrow from '../../assets/leftArrow.svg';
+import star from '../../assets/star.svg';
+import disney from '../../assets/disney.svg';
 
 interface MovieDetails {
   ageRating: string;
@@ -22,16 +25,17 @@ interface YouTubePlayerOptions {
   width?: string;
   playerVars?: {
     autoplay?: 0 | 1;
-    // ...여기에 다른 YouTube player 매개변수를 추가할 수 있습니다.
+    rel?: 0 | 1;
   };
 }
 
 const MovieTrailer = ({ videoId }: MovieTrailerProps) => {
   const opts: YouTubePlayerOptions = {
-    // height: '390',
-    // width: '640',
+    height: '100%',
+    width: '100%',
     playerVars: {
       autoplay: 0,
+      rel: 0,
     },
   };
 
@@ -42,7 +46,7 @@ const MovieTrailer = ({ videoId }: MovieTrailerProps) => {
   );
 };
 
-const MovieDetail: React.FC = () => {
+const MovieDetail = () => {
   const { movieId } = useParams<{ movieId: string }>();
   const [movieDetails, setMovieDetails] = useState<MovieDetails | null>(null);
 
@@ -70,31 +74,59 @@ const MovieDetail: React.FC = () => {
 
   return (
     <Layout>
-      <div className="max-w-md mx-auto w-full">
-        <MovieTrailer videoId={movieDetails.trailerId} />
-      </div>
-      <div>
-        <img src="/path-to-age-rating-icon.png" alt="Age Rating" />
-        <h1>{movieDetails.title}</h1>
-        <p>{movieDetails.info}</p>
-        <p>{movieDetails.story}</p>
-        <div>
-          <h2>Available on:</h2>
-          <ul>
-            {movieDetails.availableOTT.map((ott, index) => (
-              <li key={index}>{ott}</li>
-            ))}
-          </ul>
+      <header className="flex items-center justify-start h-16 mx-6">
+        <img src={leftArrow} />
+      </header>
+      <section className="flex flex-col mx-4">
+        <div className="max-w-md mx-auto w-full mb-10">
+          <MovieTrailer videoId={movieDetails.trailerId} />
         </div>
-        <div>
-          <h2>Similar Movies:</h2>
-          <ul>
-            {movieDetails.similarContent.map((content, index) => (
-              <li key={index}>{content}</li>
-            ))}
-          </ul>
+        <div className="border-b border-[#2C2D32]">
+          <div className="p-2 bg-[#129148] text-xs text-white font-medium rounded w-max">All</div>
+          <div className="mt-2.5 mb-6">
+            <h1 className="font-bold text-2xl text-white">{movieDetails.title}</h1>
+            <p className="text-xs text-white">{movieDetails.info}</p>
+            <div className="flex">
+              <span className="text-sm text-white mr-2">평점</span>
+              <img src={star} alt="star" />
+              <span className="text-sm text-white ml-0.5">2</span>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 pb-4">
+            <p className="text-xs text-white">줄거리</p>
+            <p className="text-[8px] text-white">{movieDetails.story}</p>
+          </div>
         </div>
-      </div>
+        {/* 상영가능한 OTT */}
+        <div className="mt-5">
+          <h2 className="text-xs font-bold text-white mb-3">상영가능한 OTT</h2>
+          <div className="flex">
+            <img src={disney} alt="disney" />
+          </div>
+        </div>
+        <div className="mt-6">
+          <h2 className="text-xs font-bold text-white mb-3">유사 콘텐츠</h2>
+          <div className="flex gap-x-2" />
+        </div>
+        {/* <div>
+          <div>
+            <h2>Available on:</h2>
+            <ul>
+              {movieDetails.availableOTT.map((ott, index) => (
+                <li key={index}>{ott}</li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h2>Similar Movies:</h2>
+            <ul>
+              {movieDetails.similarContent.map((content, index) => (
+                <li key={index}>{content}</li>
+              ))}
+            </ul>
+          </div>
+        </div> */}
+      </section>
     </Layout>
   );
 };
