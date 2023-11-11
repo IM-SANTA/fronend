d40f7dea06485d8908abcbc5f98f197f4abe328b
Hannah Ahn <ahnlook90@gmail.com>
2023-11-11 15:28:12 +0900

    feat: 노래 장르별 필터링

--- src/pages/musics/musicCard.tsx
+++ src/pages/musics/musicCard.tsx
@@ -1,21 +1,23 @@
 import { Link } from 'react-router-dom';
 
-interface MusicCardProps {
+export interface Music {
   id: number;
-  thumbnail: string;
   title: string;
-  creator: string;
+  url: string;
+  channel_name: string;
+  youtube_id: string;
+  categories: string[];
 }
 
-const MusicCard = ({ id, thumbnail, title, creator }: MusicCardProps) => {
+const MusicCard = ({ id, title, channel_name, youtube_id }: Music) => {
   return (
     <Link to={`/music/${id}`} className="text-decoration-none">
       <div className="overflow-hidden mb-2 scrollbar-hide">
-        <img className="rounded w-full h-[116px]" src={thumbnail} />
+        <img className="rounded w-full h-[116px]" src={`https://img.youtube.com/vi/${youtube_id}/maxresdefault.jpg`} />
       </div>
       <div className="flex flex-col gap-x-1">
         <span className="text-xl text-white">{title}</span>
-        <span className="text-sm text-white">{creator}</span>
+        <span className="text-sm text-white">{channel_name}</span>
       </div>
     </Link>
   );
