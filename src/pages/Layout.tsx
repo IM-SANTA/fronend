e83846ff59bc49d1d3b393b748b939b9cb87e885
Hannah Ahn <ahnlook90@gmail.com>
2023-11-11 18:05:32 +0900

    fix: 레이아웃 반응형  가운데 정렬

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -10,12 +10,13 @@ const Layout = ({ position = 'static', children, ...props }: LayoutProps) => {
 
   return (
     <>
-      <div {...props} className={`${styles} ${position} flex justify-center items-start min-h-screen h-screen`}>
-        {/* <div {...props} className="flex justify-center web:items-start md:items-center min-h-screen h-screen"> */}
+      <div
+        {...props}
+        className={`${styles} ${position} flex justify-center min-h-screen h-screen items-start sm:items-center`}
+      >
         <main
           className={` bg-secondary overflow-scroll scrollbar-hide w-[540px] h-[1024px] md:min-w-[540px] md:min-h-[1024px]`}
         >
-          {/* <main className="bg-secondary overflow-scroll scrollbar-hide w-[540px] h-[1024px] md:min-w-[540px] md:min-h-[1024px]"> */}
           <div>{children}</div>
         </main>
       </div>
