2abe44abad5325b474d3f64216fdec4e96ed5982
Hannah Ahn <ahnlook90@gmail.com>
2023-11-12 19:47:30 +0900

    feat: google analytics 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
 import Onboarding from './pages/Onboarding';
 
-const App = () => <Onboarding />;
+ReactGA.initialize('G-BMBF7DL2L9');
+
+const App = () => {
+  const trackPage = (page: string) => {
+    ReactGA.set({ page });
+    ReactGA.pageview(page);
+  };
+
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPage(location.pathname);
+    // 라우팅 변경 시 trackPage 함수 호출
+  }, [location]);
+
+  return <Onboarding />;
+};
 
 export default App;
